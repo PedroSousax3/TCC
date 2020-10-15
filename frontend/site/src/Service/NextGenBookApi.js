2a042511ddd2cc1f42da214a78daff05e533b275
Senhor Supremo <ramonsouzasil2004@gmail.com>
2020-10-15 20:32:32 -0300

    subindo progamação da tela login

---
+++ frontend/site/src/Service/NextGenBookApi.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const api = axios.create({
+    baseURL:"http://localhost:5000"
+})
+
+export default class NextGenBookApi{
+    async login(req){
+        const resp = await api.post('/Login/Login', req);
+        return resp;
+    }
+}
