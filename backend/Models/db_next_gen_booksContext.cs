d91c6cf3fef56cd134d57469a034404edbde3740
rebecca <revip.pop@gmail.com>
2020-10-15 12:54:27 -0300

    rota consultar categorias

--- backend/Models/db_next_gen_booksContext.cs
+++ backend/Models/db_next_gen_booksContext.cs
@@ -39,7 +39,7 @@ namespace backend.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+
                 optionsBuilder.UseMySql("server=localhost;user id=root;password=1234;database=db_next_gen_books", x => x.ServerVersion("5.7.31-mysql"));
             }
         }
