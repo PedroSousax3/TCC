5be7e27586409c6fb265f619eb4eff3fb7cc0b2f
rebecca <revip.pop@gmail.com>
2020-10-18 23:07:35 -0300

    Crud

--- backend/Database/LivroAutorDatabase.cs
+++ backend/Database/LivroAutorDatabase.cs
@@ -25,6 +25,10 @@ namespace backend.Database
            return await context.TbLivroAutor.Include(x => x.IdLivroNavigation).Include(x => x.IdAutorNavigation)
                                             .Where(x => x.IdAutorNavigation.IdAutor == id).ToListAsync();
         }
+        public Task<List<Models.TbLivroGenero>> ListarLivroGenero()
+        {
+            return context.TbLivroGenero.Include(x => x.IdGeneroNavigation).Include(x => x.IdLivroNavigation).ToListAsync();
+        }
 
         public Task<Models.TbLivroAutor> ConsultarPorIdLivroAutor(int id)
         {
