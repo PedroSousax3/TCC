54a259e274ef73ba9911e8e216295d088a9feb3f
infob29 <infob29@edu.frei>
2020-10-19 08:18:37 -0300

    Crud

---
+++ backend/Database/MedidasDatabase.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace backend.Database
+{
+    public class MedidasDatabase
+    {
+        Models.db_next_gen_booksContext context = new Models.db_next_gen_booksContext();
+
+
+        public async Task<Models.TbMedidas> CadastrarMedidas(Models.TbMedidas tabela)
+        {
+            await context.TbMedidas.AddAsync(tabela);
+            await context.SaveChangesAsync();
+            return tabela;
+        }
+
+        public Task<Models.TbMedidas> ConsultarPorIdMedidas(int id)
+        {
+           return context.TbMedidas.FirstOrDefaultAsync(x => x.IdMedidas == id);
+        }
+
+        public async Task<Models.TbMedidas> AlterarMedidas(int id,Models.TbMedidas novaTabela)
+        {
+            Models.TbMedidas tabela = await ConsultarPorIdMedidas(id);
+            tabela.VlAltura = novaTabela.VlAltura;
+            tabela.VlLargura = novaTabela.VlLargura;
+            tabela.VlPeso = novaTabela.VlPeso;
+            tabela.VlProfundidades = novaTabela.VlProfundidades;
+            await context.SaveChangesAsync();
+            return tabela;
+        }
+        
+        public async Task<Models.TbMedidas> DeletarMedidas(int id)
+        {
+            Models.TbMedidas tabela = await ConsultarPorIdMedidas(id);
+            context.TbMedidas.Remove(tabela);
+            await context.SaveChangesAsync();
+            return tabela;
+        }
+    }
+}
