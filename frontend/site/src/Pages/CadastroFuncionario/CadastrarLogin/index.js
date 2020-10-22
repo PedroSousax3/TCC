849d79470042b2a150ce354062585f9caaa0c8f5
rebecca <revip.pop@gmail.com>
2020-10-21 22:50:49 -0300

    medo de estragar o projeto :)

---
+++ frontend/site/src/Pages/CadastroFuncionario/CadastrarLogin/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import PageCadastroFuncionario from "../PageCadastroFuncionario";
+import {LoginCaixaFuncionario} from "./CadastrarLoginFuncionario/style";
+import nextGenBookAPI from "../../../Service/NextGenBookApi";
+const api = new nextGenBookAPI();
+export default function CadastrarLogin()
+{
+    
+    const navegacao = useHistory()
+    const [Nome, setNome ] = useState("");
+    const [ Email, setEmail ] = useState("");
+    const [ NomeDeUsuario, setNomeDeUsuario ] = useState("");
+    const [ Senha, setSenha ] = useState("");
+
+
+    const CadastrarLogin = async () => {
+      const request = {
+        Nome,
+        Email,
+        NomeDeUsuario,
+        Senha
+      }
+      console.log(request);
+      const a = await  api.cadastrarLoginFuncionario(request);
+      navegacao.push("/",a.data);
+      console.log(a);
+    }
+    return(
+        <div>
+
+        <PageCadastroFuncionario/>
+        <div style={{justifyContent:"center",alignItems:"center",paddingTop:"7%",display:"flex",flexDirection:"column"}}>
+            <div style={{width:"80%",display:"flex",justifyContent:"flex-start",fontSize:"25px",fontWeight:"bold"}}>
+                <span>CADASTRAR FUNCIONARIOS</span>
+            </div>
+            <LoginCaixaFuncionario>
+              <div className="agrupamento">
+                    <div className="textos">
+                        <span>Nome:</span>
+                        <span>E-mail:</span>
+                        <span>Nome do Usuario:</span>
+                        <span>Senha:</span>
+                    </div>
+                    <div className="inputs">
+                        <input type="text" onChange={(n) => setNome(n.target.value)}></input>
+                        <input type="text" onChange={(e) => setEmail(e.target.value)}></input>
+                        <input type="text" onChange={(n) => setNomeDeUsuario(n.target.value)}></input>
+                        <div className="input-botao">
+                        <input type="password"onChange={(s) => setSenha(s.target.value)}></input>
+                            <button
+                                    onClick={() => document.getElementById("Senha").type === "password" 
+                                    ? document.getElementById("Senha").type = "text"
+                                    : document.getElementById("Senha").type = "password"
+
+                                    }
+                            >Mostrar</button>
+                        </div>
+                    </div>
+                </div> 
+                <div className="botao-next">
+                    <button onClick={CadastrarLogin}>Próximo {">"}</button>    
+                </div> 
+            </LoginCaixaFuncionario>
+        </div>
+        </div>
+    )
+}
--- frontend/site/src/Route.js
+++ frontend/site/src/Route.js
@@ -5,6 +5,7 @@ import Login from './Pages/Login/index.js';
 import Home  from './Pages/Home';
 import Cadastro from './Pages/Cadastro';
 import EsqueciSenha from './Pages/EsqueciSenha';
+import CadastrarLogin from './Pages/CadastroFuncionario/CadastrarLogin'
 
 function Rotas(){
     return(
@@ -14,6 +15,9 @@ function Rotas(){
           <Route path="/Login" component={Login}/>
           <Route path="/Cadastro" component={Cadastro}/>
           <Route path="/EsqueciSenha" component={EsqueciSenha}/>
+
+          <Route path="/Funcionario/Cadastro/Login" component={CadastrarLogin}/>
+
         </Switch>
       </BrowserRouter>
     )
