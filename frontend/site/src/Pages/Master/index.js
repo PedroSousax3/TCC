69880a84d865382f5ac0d081f9c4c6b0bf17ae58
PedroSousax3 <pedro.sousax3@gmail.com>
2020-11-01 12:43:35 -0300

    Subindo Pagina de Mostrar Livro Responsive - So falta conectar a api

--- frontend/site/src/Pages/Master/index.js
+++ frontend/site/src/Pages/Master/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Menu from '../../components/Menu/index'
 import { MasterPage, ContainerPage } from './style.js';
 
+import { MenuSpace } from '../../components/Utils/index.js'
+
 export default function Master(props) {
     return (
         <MasterPage>
+            <MenuSpace theme={{sc_height : "60px"}}/>
             <Menu />
             
             <ContainerPage>
                 {props.children}
             </ContainerPage>
+            <MenuSpace id="menu-baixo" theme={{sc_height : "60px"}}/>
         </MasterPage>
     );
 }
