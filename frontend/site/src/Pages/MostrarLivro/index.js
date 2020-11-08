fac5ae6868911e41ce88bb17cde3112358f6745a
PedroSousax3 <pedro.sousax3@gmail.com>
2020-11-08 15:59:53 -0300

    Subindo Resto

--- frontend/site/src/Pages/MostrarLivro/index.js
+++ frontend/site/src/Pages/MostrarLivro/index.js
@@ -14,27 +14,27 @@ import { ConsultarPorIdLivro } from '../../Service/LivroApi.js';
 import { BuscarFoto } from '../../Service/fileApi.js'
 
 export default function MostrarLivro(props) {
-    const [ id ] = useState(props.location.state.idlivro);
-    const [ nome, setNome] = useState("");
-    const [ idcliente, setIdCliente] = useState(1);
-    const [ valor, setValor ] = useState();
-    const [ edicao, setEdicao ] = useState();
-    const [ acabamento, setAcabamento ] = useState("");
-    const [ altura, setAltura ] = useState();
-    const [ peso, setpeso ] = useState();
-    const [ largura, setLargura ] = useState();
-    const [ lancamento, setLancamento ]= useState();
-    const [ descricao, setDescricao] = useState("");
-    const [ paginas, setPaginas ] = useState();
-    const [ editora, setEditora ] = useState("");
-    const [ autor, setAutor ] = useState([]);
-    const [ genero, setGenero ] = useState("");
-    const [ qtd, setQtd ] = useState(0);
-    const [ foto, setFoto ] = useState("");
+    const [id] = useState(props.location.state.idlivro);
+    const [nome, setNome] = useState("");
+    const [idcliente, setIdCliente] = useState(1);
+    const [valor, setValor] = useState();
+    const [edicao, setEdicao] = useState();
+    const [acabamento, setAcabamento] = useState("");
+    const [altura, setAltura] = useState();
+    const [peso, setpeso] = useState();
+    const [largura, setLargura] = useState();
+    const [lancamento, setLancamento] = useState();
+    const [descricao, setDescricao] = useState("");
+    const [paginas, setPaginas] = useState();
+    const [editora, setEditora] = useState("");
+    const [autor, setAutor] = useState([]);
+    const [genero, setGenero] = useState("");
+    const [qtd, setQtd] = useState(0);
+    const [foto, setFoto] = useState("");
 
-    
-    function popularLivro (dados) {
-        if(dados.livro != null && dados.livro != undefined) {
+
+    function popularLivro(dados) {
+        if (dados.livro != null && dados.livro != undefined) {
             setLancamento(new Date(dados.livro.lancamento).toLocaleDateString());
             setNome(dados.livro.nome);
             setValor(dados.livro.venda);
@@ -43,27 +43,27 @@ export default function MostrarLivro(props) {
             setPaginas(dados.livro.paginas)
             setDescricao(dados.livro.descricao);
             setFoto(dados.livro.foto);
-            if(dados.livro.editora != null && dados.livro.editora != undefined)
-            setEditora(dados.livro.editora.nome);
-            if(dados.livro.medida != null && dados.livro.editora != undefined){
+            if (dados.livro.editora != null && dados.livro.editora != undefined)
+                setEditora(dados.livro.editora.nome);
+            if (dados.livro.medida != null && dados.livro.editora != undefined) {
                 setAltura(dados.livro.medida.altura)
                 setLargura(dados.livro.medida.largura)
                 setpeso(dados.livro.medida.peso)
             }
         }
-        if(dados.estoque_livro != null && dados.estoque_livro != undefined)
+        if (dados.estoque_livro != null && dados.estoque_livro != undefined)
             setQtd(dados.estoque_livro.qtd);
-        if(dados.generos != null && dados.generos != undefined)
+        if (dados.generos != null && dados.generos != undefined)
             setGenero(dados.generos.map(x => x.genero + " ").toString());
         setAutor([...dados.autores]);
     }
 
-    async function inserirCarrinho(){
+    async function inserirCarrinho() {
         try {
             let request = {
-                livro : id,
-                cliente : idcliente,
-                qtd : 1
+                livro: id,
+                cliente: idcliente,
+                qtd: 1
             }
             await InserirCarrinhoApi(request);
             toast.success('Livro foi adicionado ao carrinho com sucesso');
@@ -72,20 +72,20 @@ export default function MostrarLivro(props) {
             toast.error(ex.response.data.erro);
         }
     }
-    
-    async function inserirFavorito(){
+
+    async function inserirFavorito() {
         try {
             await inserirFavoritoApi({
-                livro : id,
-                cliente : idcliente
+                livro: id,
+                cliente: idcliente
             });
             toast.success('Livro foi adicionado a lista de favoritos com sucesso');
         } catch (ex) {
-            toast.error("ex.response.data.erro");    
+            toast.error("ex.response.data.erro");
         }
     }
-    
-    async function Consultar (){
+
+    async function Consultar() {
         const response = await ConsultarPorIdLivro(id);
         console.log(response);
         popularLivro(response.data);
@@ -94,43 +94,44 @@ export default function MostrarLivro(props) {
     useEffect(() => {
         Consultar();
     }, [])
-    
+
     return (
         <Master>
-            <BoxContainer id="livro" theme={{ sc_border : "3.5px solid #00870D", sc_espace : "80px 80px", sc_padding : "10px", sc_direction : "column"}}>
-                <BoxContainer id="titulo" theme={{sc_espace : "10px 0px", sc_direction : "row"}}>
+            <BoxContainer id="livro" theme={{ sc_border: "3.5px solid #00870D", sc_espace: "80px 80px", sc_padding: "10px", sc_direction: "column" }}>
+                <BoxContainer id="titulo" theme={{ sc_espace: "10px 0px", sc_direction: "row" }}>
                     <h2>{nome}</h2>
-                    <i className="fa fa-star estrela" onClick={inserirFavorito}></i>
+                    <i className="fa fa-star estrela" onClick={inserirFavorito} style={{cursor:"pointer"}}></i>
                 </BoxContainer>
-                <BoxContainer id="generico" theme={{sc_espace : "10px 0px", sc_direction : "row"}}>
-                    <BoxContainer id="imagem" theme={{sc_espace : "10px 0px", sc_direction : "column"}}>
-                        <img src={BuscarFoto(foto)} width="220px" alt=""/>
+                <BoxContainer id="generico" theme={{ sc_espace: "10px 0px", sc_direction: "row" }}>
+                    <BoxContainer id="imagem" theme={{ sc_espace: "10px 0px", sc_direction: "column" }}>
+                        <img src={BuscarFoto(foto)} width="220px" alt="" />
                         <div>
                             <p>Quantidade Disponivel: {qtd}</p>
                         </div>
                     </BoxContainer>
-                    <BoxContainer id="itemgenerico" theme={{sc_width: "100%", sc_espace : "10px 0px", sc_direction : "column"}}>                        
+                    <BoxContainer id="itemgenerico" theme={{ sc_width: "100%", sc_espace: "10px 0px", sc_direction: "column" }}>
+                        <h5 style={{ marginTop: "15px", marginBottom: "5px" }}>Descrição do Livro</h5>
                         <div>
-                            <div className="style-text-descr">Editora: {editora}</div>
-                            <div className="style-text-descr">Autor: {autor.map(x => x.nome).toString()}</div>
-                            <div className="style-text-descr">Generos: {genero}</div>
+                            {descricao}
                         </div>
-                        <div className="style-text-descr finalitem">Valor Unitario: {valor}</div>
                     </BoxContainer>
                 </BoxContainer>
-                <BoxContainer id="acoes" theme={{sc_espace : "10px 0px"}}>
+                <BoxContainer id="acoes" theme={{ sc_espace: "10px 0px" }}>
                     <button type="button" className="btn btn-carrinho" onClick={inserirCarrinho}>
                         Adicionar ao Carrinho
                     </button>
                     <button type="button" className="btn btn-comprar">Comprar</button>
                 </BoxContainer>
-                <BoxContainer id="descicao" theme={{sc_espace : "10px 0px", sc_direction : "column"}}>
-                    <h5 style={{marginTop: "15px", marginBottom: "5px"}}>Descrição do Livro</h5>
-                    <div>
-                        {descricao}
-                    </div>
+                <BoxContainer id="descicao" theme={{ sc_espace: "10px 0px", sc_direction: "column" }}>
 
-                    <h5 style={{marginTop: "15px", marginBottom: "5px"}}>Sobre o Escritor</h5>
+                    <div>
+                        <div className="style-text-descr">Editora: {editora}</div>
+                        <div className="style-text-descr">Autor: {autor.map(x => x.nome).toString()}</div>
+                        <div className="style-text-descr">Generos: {genero}</div>
+                    </div>
+                    <div className="style-text-descr finalitem">Valor Unitario: {valor}</div>
+
+                    <h5 style={{ marginTop: "15px", marginBottom: "5px" }}>Sobre o Escritor</h5>
                     <div>
                         {autor.map(x =>
                             <div>
@@ -144,8 +145,8 @@ export default function MostrarLivro(props) {
                         )}
                     </div>
                 </BoxContainer>
-                <h5 style={{marginTop: "15px", marginBottom: "5px"}}>Informações do Livro</h5>
-                <BoxContainer id="informacoes" theme={{sc_espace : "10px 0px"}}>
+                <h5 style={{ marginTop: "15px", marginBottom: "5px" }}>Informações do Livro</h5>
+                <BoxContainer id="informacoes" theme={{ sc_espace: "10px 0px" }}>
                     <ul>
                         <li>Número de paginas: {paginas}</li>
                         <li>Edição: {edicao}º</li>
--- frontend/site/src/Pages/Perfil/index.js
+++ frontend/site/src/Pages/Perfil/index.js
@@ -7,41 +7,41 @@ import { CaixaPerfil } from './style';
 import Master from '../Master/index.js';
 
 
-export default function EsqueciSenha(props) {
+export default function EsqueciSenha(props){
 
     const [infos, setInfos] = useState(props.location.state);
 
-    return (
-        <Master>
-                <div style={{ justifyContent: "center", alignItems: "center", paddingTop: "3%", display: "flex", flexDirection: "column" }}>
-                    <div style={{ width: "80%", display: "flex", justifyContent: "flex-start", fontSize: "25px", fontWeight: "bold" }}>
-                    </div>
-                    <CaixaPerfil>
-                        <div style={{ width: "100%", height: "50vh", backgroundColor: "#98F0BB", display: "flex", borderRadius: "10px" }}>
-                            <div className="foto" style={{ width: "20%", height: "35vh", alignItems: "center", justifyContent: "center", display: "flex" }}>
-                                <div style={{ width: "80%", height: "30vh", backgroundColor: "white", borderRadius: "10px" }}></div>
-                            </div>
-                            <div className="informacoes" style={{ width: "60%", height: "35vh" }} >
-                                <div style={{ marginTop: "10px" }}>NOME:</div>
-                                <div>IDADE:</div>
-                                <div>GENERO:</div>
-
-                            </div>
-
-                            <div className="botoes" style={{ width: "20%", height: "35vh" }}>
-                                <div style={{ marginTop: "10px", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
-                                    <Link to="Alterar-Dados" className="btn btn-success" style={{ marginTop: "5px" }}>ALTERAR DADOS DA CONTA</Link>
-                                    <button className="btn btn-success" style={{ marginTop: "7px" }}>IR PARA SEUS FAVORITOS</button>
-                                    <button className="btn btn-success" style={{ marginTop: "7px" }}>CADASTRAR ENDEREÇO</button>
+    return(
+       <Master>
+           <div>
+                   <div style={{justifyContent:"center",alignItems:"center",paddingTop:"3%",display:"flex",flexDirection:"column"}}>
+                        <div style={{width:"80%",display:"flex",justifyContent:"flex-start",fontSize:"25px",fontWeight:"bold"}}>
+                                </div>
+                        <CaixaPerfil>
+                            <div style={{width:"100%", height:"50vh", backgroundColor:"#98F0BB", display:"flex", borderRadius:"10px"}}>
+                                <div className="foto" style={{width:"20%", height:"35vh", alignItems:"center", justifyContent:"center", display:"flex"}}>
+                                    <div style={{width:"80%", height:"30vh", backgroundColor:"white", borderRadius:"10px"}}></div>
+                                </div>
+                                <div className="informacoes" style={{width:"60%", height:"35vh" }} >
+                                    <div style={{marginTop:"10px"}}>NOME:</div>
+                                    <div>IDADE:</div>
+                                    <div>GENERO:</div>
+                                    
+                                </div>
+                                
+                                <div className="botoes" style={{width:"20%", height:"35vh"}}>
+                                    <div style={{marginTop:"10px", display:"flex", alignItems:"center", justifyContent:"center", flexDirection:"column"}}>
+                                        <button className="btn btn-success" style={{marginTop:"5px"}}>ALTERAR DADOS DA CONTA</button>
+                                        <button className="btn btn-success" style={{marginTop:"7px"}}>IR PARA SEUS FAVORITOS</button>
+                                    z    <button className="btn btn-success" style={{marginTop:"7px"}}>CADASTRAR ENDEREÇO</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div style={{ width: "100%", height: "50vh", borderRadius: "10px" }}>
-                            Fugiat dolore ad ea quis sunt amet ad sint adipisicing incididunt quis incididunt sunt consequat.
-                        </div>
-                    </CaixaPerfil>
-                    <ToastContainer />
+                            <div style={{width:"100%", height:"50vh",  borderRadius:"10px"}}>b</div>
+                        </CaixaPerfil>
+                        <ToastContainer />
+                    </div>
                 </div>
-        </Master>
+       </Master>
     );
 }
