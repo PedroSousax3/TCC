e5d18785f02e479008a4d9b448810847f6d01af1
PedroSousax3 <pedro.sousax3@gmail.com>
2020-11-11 21:01:10 -0300

    Subindo alteração api

--- frontend/site/src/Service/favoritosApi.js
+++ frontend/site/src/Service/favoritosApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create(
-    {   baseURL : 'http://localhost:5000/Favoritos'  }
+    {   baseURL : 'http://localh:5000/Favoritos'  }
 );
 
 export const listarApi = async (idcliente) => {
