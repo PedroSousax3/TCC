817c36e55998a56eda9ab8ee769034490b039efd
rebecca <revip.pop@gmail.com>
2020-11-22 18:00:06 -0300

    ajustando

--- frontend/NextGenBooks/src/Pages/Cliente/FinalizarCompra/style.js
+++ frontend/NextGenBooks/src/Pages/Cliente/FinalizarCompra/style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 export const CaixaFinalizarCompra = styled.div`
-    background-color: rgb(152, 240, 187);
+    background-color: #fff;
     width: 60vw;
     min-height:50vh;
     display: flex;
@@ -12,12 +12,13 @@ export const CaixaFinalizarCompra = styled.div`
     padding-bottom:1%;
     font-weight:bold;
     border:none;
-    color:#D26E4E;
+    color:green;
+    font-family: Arial,Helvetica,"Nimbus Sans L",sans-serif;
     border-radius:10px;
     box-shadow:10px 10px 5px 0px rgb(0,0,0,0.2);
 .table{
     width:100%;
-    color:#00870D;
+    color:#999;
 }
 .form-group{
     display:flex;
@@ -39,7 +40,7 @@ export const CaixaFinalizarCompra = styled.div`
 .botao button{
     margin-top:10%;
     width:35%;
-    background-color:#D26E4E;
+    background-color:green;
     color:white;
     font-weight:bold;
 }
