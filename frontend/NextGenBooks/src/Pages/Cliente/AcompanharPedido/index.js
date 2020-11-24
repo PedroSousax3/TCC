023d7d6dba9a1d773974c7baa90b30cbe24b9082
rebecca <revip.pop@gmail.com>
2020-11-23 22:22:15 -0300

    ajustando

--- frontend/NextGenBooks/src/Pages/Cliente/AcompanharPedido/index.js
+++ frontend/NextGenBooks/src/Pages/Cliente/AcompanharPedido/index.js
@@ -1,32 +1,42 @@
 import React, { useState } from "react";
 import { Container, CaixaPadrao } from "./style.js";
 import { rastrearPedido } from "../../../Service/ApiCorreio.js";
+import { ToastContainer, toast } from "react-toastify";
 import Master from "../../Master";
 
 export default function AcompanharPedido() {
     const [registros, setRegistros] = useState([]);
     const [valor, setValor] = useState("");
-
+    const [erro,setErro] = useState("");
+    
     const acompanharPedido = async () => {
         try {
             let resp = await rastrearPedido(valor);
-            console.log(resp.map(x => x))
-            if(resp.length > 0)
+            if(resp.includes("Cannot GET /rastrear/" || resp.lenght < 1 || valor === ""))
+            {
+                setErro("Encomenda não encontrada, verifique se o código está correto")
+                let zerado = [];
+                setRegistros([...zerado]);
+               
+            }else{
+
                 setRegistros([...resp]);
+            } 
         } catch (e) {
             console.error(e);
         }
     }
+
     return (
         <Master>
             <Container>
                 <CaixaPadrao>
                     <div style={{ textAlign: "center" }}>
-                        <h3 style={{ color: "green" }}>Status do envio</h3>
+                        <h3 style={{ color: "green",marginBottom:"6%" }}>Status do envio</h3>
                     </div>
-                    <div className="form-group row" style={{ width: "80%", display: "flex" }} >
-                        <div className="col-sm-10" style={{ display: "flex", flexDirection: "row", width: "80%" }}>
-                            <input type="text" style={{ width: "50%", marginRight: "5px" }} className="form-control" id="codigo" placeholder="Digite o codigo" onChange={(e) => setValor(e.target.value)} />
+                    <div className="form-group row" style={{ width: "80%",display:"flex",justifyContent:"center",alignItems:"center"}} >
+                        <div className="col-sm-10" style={{ display: "flex", flexDirection: "row", width: "80%" ,display:"flex",justifyContent:"center",alignItems:"center"}}>
+                            <input type="text" style={{ width: "50%", marginRight: "5px",marginLeft:"25%" }} className="form-control" id="codigo" placeholder="Digite o codigo" onChange={(e) => setValor(e.target.value)} />
                             <div className="button1">
                                 <button className="btn btn-success" onClick={acompanharPedido}>Buscar</button>
                             </div>
@@ -56,9 +66,18 @@ export default function AcompanharPedido() {
                                 </div>
                             )
                         )}
+                        { registros.length < 1 &&
+                           <div>
+                               <h5 style={{color:"gray",marginLeft:"5%"}}>
+                                  {erro} 
+                               </h5>
+                               </div>
+
+                        }
                     </div>
                 </CaixaPadrao>
             </Container>
+            <ToastContainer />
         </Master>
     )
 }
