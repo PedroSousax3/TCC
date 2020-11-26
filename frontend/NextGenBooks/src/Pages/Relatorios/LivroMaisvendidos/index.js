d848101a40fb396a6faf16e0b1c68e21dc5f6e6b
PedroSousax3 <pedro.sousax3@gmail.com>
2020-11-26 08:10:16 -0300

    Subindo ajuste relatorio

--- frontend/NextGenBooks/src/Pages/Relatorios/LivroMaisvendidos/index.js
+++ frontend/NextGenBooks/src/Pages/Relatorios/LivroMaisvendidos/index.js
@@ -34,8 +34,13 @@ export default function LivroMaisvendidos() {
         const container = document.getElementById("relatotio").innerHTML;
 
         let screen = `
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />            
-            ${container}
+            <head>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+            </head>
+
+            <body>
+                ${container}
+            </body>
         `;
 
         const tela = window.open('about:blank');
